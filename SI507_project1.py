6175b044e70aaf1f083e9d0ac441a98a72335555
BingqingShan <bqshan@umich.edu>
2019-02-06 21:29:08 -0500

    finish debugging my code

--- SI507_project1.py
+++ SI507_project1.py
@@ -9,25 +9,37 @@ def home():
 
 @app.route('/bank/<name>')
 def bankname(name):
-    one_bank= Bank(name).name
-    return:'Welcome to'+ one_bank.__repr__()
+    bank_name= Bank(name,Dollar)
+    return 'Welcome to '+ bank_name.name +'!'
 
 @app.route('/dollar/<amt>')
 def dollaramount(amt):
     dollar_amount= Dollar(int(amt))
-    return: dollar_amount.__repr__()
-
-@app.route('/yuan/<amt>')
-def yuanamount(amt):
-    yuan_amount= Yuan(int(amt))
-    return: yuan_amount.__repr__()
+    return dollar_amount.__str__()
 
 @app.route('/pound/<amt>')
 def poundamount(amt):
     pound_amount= Pound(int(amt))
-    return: pound_amount.__repr__()
+    return pound_amount.__str__()
+
+@app.route('/yuan/<amt>')
+def yuanamount(amt):
+    yuan_amount= Yuan(int(amt))
+    return yuan_amount.__str__()
 
 @app.route('/bank/<name>/<currency>/<value>')
-def bankinfo(name,  currency, value):
-    bank_info= Bank(name,  currency, int(value))
-    return: 'Welcome to the NAME bank!'+bank_info.__repr__()
+def bankinfo(name,currency,value):
+    if currency =="Dollar":
+        bank_info=Bank(name,Dollar,int(value))
+        return 'Welcome to the '+bank_info.name+' bank! '+bank_info.__str__()
+    elif currency =="Pound":
+        bank_info=Bank(name,Pound,int(value))
+        return 'Welcome to the '+bank_info.name+' bank! '+bank_info.__str__()
+    elif currency =="Yuan":
+        bank_info=Bank(name,Yuan,int(value))
+        return 'Welcome to the '+bank_info.name+' bank! '+bank_info.__str__()
+    else:
+        return 'Invalid URL inputs for bank.'
+
+if __name__ == "__main__":
+        app.run()
